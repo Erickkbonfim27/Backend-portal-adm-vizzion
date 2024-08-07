740fc962b8bc2bbec0b36b8f2b8f537e5e90a0fe
Erick Kopak Bonfim  -  Vizzion <141456725+Erickkbonfim27@users.noreply.github.com>
2024-08-07 17:19:18 -0300

    first commit

---
+++ portaladm/src/main/java/com/vizzion/portaladm/PortalInternoVizzionApplication.java
@@ -0,0 +1,13 @@
+package com.vizzion.portaladm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalInternoVizzionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalInternoVizzionApplication.class, args);
+	}
+
+}
---
+++ portaladm/src/test/java/com/vizzion/portaladm/PortalInternoVizzionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizzion.portaladm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalInternoVizzionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
